55131b715b704611d1d789bb09e86308dbe6e50d
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-20 16:39:24 +0600

    user register api create and password validation added

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,21 +1,26 @@
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 const User = require("../models/usersModels");
 const jwt = require("jsonwebtoken");
+const { signupSchema } = require("../middlewares/validator");
 
-//  (Register) 
+//  (Register)
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
   try {
+    const { error } = signupSchema.validate({email, password});
+    if(error){
+      return res.status(401).send({ message : error.details[0].message})
+    }
     let user = await User.findOne({ email });
     if (user) return res.status(400).send({ message: "User already exists" });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user = new User({ name, email, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    user = new User({ email, password: hashedPassword });
 
-    await user.save();
-    res.status(201).json({ message: "User registered successfully" });
+    const result = await user.save();
+    res.status(201).send({ message: "User registered successfully", result });
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error });
+    res.status(500).send({ message: "Server Error", error });
   }
 };
 
@@ -23,9 +28,9 @@ const registerUser = async (req, res) => {
 const getUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.send(users);
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).send({ message: "Server Error" });
   }
 };
 
@@ -33,11 +38,11 @@ const getUsers = async (req, res) => {
 const getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).send({ message: "User not found" });
 
-    res.json(user);
+    res.send(user);
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).send({ message: "Server Error" });
   }
 };
 
@@ -45,16 +50,16 @@ const getUserById = async (req, res) => {
 const updateUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).send({ message: "User not found" });
 
     const { name, email } = req.body;
     user.name = name || user.name;
     user.email = email || user.email;
 
     await user.save();
-    res.json({ message: "User updated successfully", user });
+    res.send({ message: "User updated successfully", user });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).send({ message: "Server Error" });
   }
 };
 
@@ -62,12 +67,12 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).send({ message: "User not found" });
 
     await user.deleteOne();
-    res.json({ message: "User deleted successfully" });
+    res.send({ message: "User deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).send({ message: "Server Error" });
   }
 };
 
@@ -77,18 +82,18 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user)
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).send({ message: "Invalid email or password" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).send({ message: "Invalid email or password" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
     res.json({ message: "Login successful", token });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).send({ message: "Server Error" });
   }
 };
 
